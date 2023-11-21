7162425e24edd86befc07036eb8d5960afd76560
Imronshoh <monikapi2022@gmail.com>
2023-11-21 21:25:38 +0500

    a

---
+++ js/main.js
@@ -0,0 +1,9 @@
+alert("Welcome to this site!");
+
+var a = prompt("Please enter your name");
+var b = prompt("Please enter your age");
+var c = prompt("Please enter your email");
+
+console.log(a);
+console.log(b);
+console.log(c);
